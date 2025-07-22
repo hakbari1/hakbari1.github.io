c73a2b226ab5ad4a5317a3e33622b70fe2095726
hakbari1 <hakbari1@my.waketech.edu>
2025-07-21 23:09:18 -0400

    debug file added

--- Public/gradebook.js
+++ Public/gradebook.js
@@ -2,6 +2,28 @@
 function fetchGradeData(){
     // This function will query the PostgreSQL database and return grade data
     console.log("fetching grade Data...");
+    // Create a new request for HTTP data
+    let xhr = new XMLHTTPRequest();
+    // This is the address on the machine we're asking for data
+    let apiRoute = "/api/grades"
+    // when the request changes status, we run this anonymous function
+    xhr.onreadystatechange = function(){
+        let results;
+        //check if we're done
+        if(xhr.readystate === xhr.Done){
+            //check if we're successful
+            if(xhr.status !== 200){
+                console.error(`could not get grades.
+                    status: ${xhr.status}`
+                );
+            }
+         // And then call the fuction to update the HTML with our data
+         populategradebook(JSON.parse(xhr.responseText));  
+        }
+
+    }.bind(this)
+    xhr.open("get", apiRoute, true);
+    xhr.send();
 }
 
 // TODO: populate the table with grade data
